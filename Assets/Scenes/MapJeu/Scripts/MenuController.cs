603276fa625e29aa867a391f76511d565c82d42c
Dyx95 <aramaharivo73@gmail.com>
2026-02-09 22:35:00 +0100

    feat: ajout bouton menu

--- Assets/Scenes/MapJeu/Scripts/MenuController.cs
+++ Assets/Scenes/MapJeu/Scripts/MenuController.cs
@@ -1,21 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class MenuController : MonoBehaviour
 {
     public GameObject menuCanvas;
+    public GameObject menuButton;      // Bouton pour ouvrir le menu
+    public GameObject joystick;        // Joystick de déplacement
+    public GameObject interactButton;  // Bouton d'interaction
 
     void Start()
     {
         menuCanvas.SetActive(false);
     }
 
-    void Update()
+    public void ToggleMenu()
     {
-        if (Input.GetKeyDown(KeyCode.Tab))
-        {
-            menuCanvas.SetActive(!menuCanvas.activeSelf);
-        }
+        bool isOpen = !menuCanvas.activeSelf;
+
+        menuCanvas.SetActive(isOpen);
+        joystick.SetActive(!isOpen);
+        interactButton.SetActive(!isOpen);
     }
 }
