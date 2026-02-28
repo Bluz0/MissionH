62504da950e2553ac01812b3e824bb70fab157d3
Bluz0 <amouats5@gmail.com>
2026-02-28 14:28:12 +0100

    fix : Modif serveur.js pour docker

--- app_admin/src/server.js
+++ app_admin/src/server.js
@@ -1,11 +1,13 @@
-require('dotenv').config(); // Charge les variables du fichier .env
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 
 const dbUser = process.env.DB_USER;
 const dbPass = process.env.DB_PASSWORD;
 const dbHost = process.env.DB_HOST;
 const dbName = process.env.DB_NAME;
 
-const uri = `mongodb://${dbUser}:${dbPass}@${dbHost}:27017/${dbName}?authSource=admin`;
+const uri = `mongodb://admin:password@mongodb:27017/game_database?authSource=admin`;
 
 mongoose.connect(uri)
   .then(() => console.log("Connecté à MongoDB sur le serveur distant !"))
