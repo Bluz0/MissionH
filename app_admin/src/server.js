4364c83356b6802e58709cd01bf63aa5912f87fc
Bluz0 <amouats5@gmail.com>
2026-04-08 21:33:29 +0200

    fix : download devrait fonctionner

--- app_admin/src/server.js
+++ app_admin/src/server.js
@@ -60,10 +60,6 @@ mongoose.connect(uri)
 // Route de login simple
 app.post('/api/login', (req, res) => {
     const { password } = req.body;
-    
-    
-    console.log("MDP Reçu:", password);
-    console.log("MDP Attendu (env):", process.env.ADMIN_PASSWORD);
 
     if (password === process.env.ADMIN_PASSWORD) {
         res.json({ success: true, token: "authorized_access_key" });
